a59ee6c2cfaefc68c249949028f1353f8005a570
Giancarmine <giancarmine.cuofano@gmail.com>
2019-12-25 21:42:31 +0000

    Heroku branch mirror

---
+++ src/main/kotlin/ges/example/kotlinserver/app.kt
@@ -0,0 +1,39 @@
+package ges.example.kotlinserver
+
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val port = System.getenv("PORT")?.toInt() ?: 23567
+    embeddedServer(Netty, port) {
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
+        routing {
+            get("") {
+                call.respond("I'm alive!")
+            }
+
+            get("hello") {
+                call.respond(HttpStatusCode.Accepted, "Hello")
+            }
+
+            get("random/{min}/{max}") {
+                val min = call.parameters["min"]?.toIntOrNull() ?: 0
+                val max = call.parameters["max"]?.toIntOrNull() ?: 10
+                val randomString = "${(min until max).shuffled().last()}"
+                call.respond(mapOf("value" to randomString))
+            }
+        }
+    }.start(wait = true)
+}
